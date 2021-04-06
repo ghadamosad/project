5f3820f835b93c7bcc08dc4747ce103f37d419de
unknown <ghadamosad0@gmail.com>
2021-04-06 23:10:43 +0200

    gg

---
+++ js/blang.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        
+        var scrolTop= $(".scroll-top")
+         
+         if($(window).scrollTop() >=1000){
+             
+             if(scrolTop.is(':hidden'))
+             
+            scrolTop.fadeIn(500);
+         } 
+         else{
+              scrolTop.fadeOut(500);
+         }
+         
+     });
+
+});
